7209060cab72a60ab68913f018f772ed7e17b3eb
404_found <118839559+Shivanshu002@users.noreply.github.com>
2023-07-07 14:36:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {   content: ['./src/**/*.{html,js}'],   theme: {}, }; 
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html'],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors:{
+         cyan:colors.cyan,
+         teal:colors.teal
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
